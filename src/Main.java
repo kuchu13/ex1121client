f9825a51a1f4e1baedcfac0ee3c5902d5816da0e
kuchu13 <105021033@live.asia.edu.tw>
2017-11-21 13:01:23 +0800

    1121

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte buff[ ]=new byte[1024];
+        try{
+            System.out.println("正在與伺服器建立連線...");
+
+
+
+        }
+
+
+
+
+
+
+
     }
 }
